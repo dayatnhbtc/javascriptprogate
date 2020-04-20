93afbe6ccb384eb06ad4358b1f8b18ad6b52b84d
Hidayatullah <34360414+dayatnhbtc@users.noreply.github.com>
2020-04-20 17:58:28 +0700

    Add files via upload

---
+++ Javascript7.js
@@ -0,0 +1,11 @@
+// CALLBACK FUNCTION 
+
+
+const call = (callback) => {
+    callback("Ninja Ken", 14);
+  };
+  
+  // Tambahkan sebuah function yang menerima dua argument didalam argument call
+  call((name, age) => {
+    console.log(`${name} berusia ${age} tahun.`);
+  });
